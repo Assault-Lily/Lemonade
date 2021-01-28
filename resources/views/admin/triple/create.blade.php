018d1ba1b8bf26a154d48ce5e69a02c69fe50844
K Miyano <miyano@miyacorata.net>
2021-01-29 03:20:25 +0900

    Add old() and display errors

--- resources/views/admin/triple/create.blade.php
+++ resources/views/admin/triple/create.blade.php
@@ -36,11 +36,12 @@
                 <div id="triple">
                     <label>
                         主語(リリィID)
-                        <input type="number" name="lily" @if(!empty($lily))value="{{ $lily->id }}" readonly @endif>
+                        <input type="number" name="lily" required
+                               @if(!empty($lily))value="{{ $lily->id }}" readonly @else value="{{ old('lily') }}" @endif>
                     </label>
                     <label>
                         述語
-                        <input type="text" name="predicate" list="predicates">
+                        <input type="text" name="predicate" list="predicates" required value="{{ old('predicate') }}">
                         <datalist id="predicates">
                             @foreach(config('triplePredicate') as $key => $predicate)
                                 <option value="{{ $key }}">{{ $predicate }}</option>
@@ -49,15 +50,23 @@
                     </label>
                     <label>
                         目的語(オブジェクト)
-                        <input type="text" name="object">
+                        <input type="text" name="object" required value="{{ old('object') }}">
                     </label>
                 </div>
                 <p>
                     <label>
                         ネタバレとしてマークする
-                        <input type="checkbox" name="spoiler">
+                        <input type="checkbox" name="spoiler" {{ old('spoiler') ? 'checked' : ''}}>
                     </label>
                 </p>
+                @if($errors->any())
+                    <hr>
+                    <div style="color: darkred">
+                        @foreach($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                @endif
                 <hr>
                 <div class="buttons">
                     <a href="{{ url()->previous() }}" class="button">キャンセル</a>
