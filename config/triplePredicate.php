be638795462ebc09a74068dc9a3929a466f769db
K Miyano <miyano@miyacorata.net>
2021-01-28 12:28:08 +0900

    [WIP] Create triplePredicate config

---
+++ config/triplePredicate.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'birthday' => '誕生日',
+    'blood_type' =>'血液型',
+    'garden.name' => 'ガーデン名',
+    'garden.grade' => '学年',
+    'legion.name' => '所属レギオン',
+    'introduction' => '公式紹介',
+    'miyadoc' => 'MiyaDoc記事名',
+];
