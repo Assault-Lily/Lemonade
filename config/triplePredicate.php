54154ff9d155f971ad7389309ffe06537a746f7d
K Miyano <miyano@miyacorata.net>
2021-01-30 03:24:08 +0900

    Add predicates

--- config/triplePredicate.php
+++ config/triplePredicate.php
@@ -1,10 +1,34 @@
 <?php
 return [
     'birthday' => '誕生日',
-    'blood_type' =>'血液型',
-    'garden.name' => 'ガーデン名',
-    'garden.grade' => '学年',
-    'legion.name' => '所属レギオン',
+    'bloodType' =>'血液型',
+    'age' => '年齢',
+    'height' => '身長',
+    'weight' => '体重',
+    'skill_hobby' => '特技・趣味',
+    'favorite' => '好きなもの',
+    'notGood' => '苦手なもの',
+    'charm' => '主な使用CHARM',
+    'rareSkill' => 'レアスキル',
+    'subSkill' => 'サブスキル',
+    'tacticalPosition' => '戦術ポジション',
+    'partner' => [
+        'schutzengel' => 'シュッツエンゲル',
+        'schild' => 'シルト',
+        'schwester' => 'シュベスター',
+    ],
+    'garden' => [
+        'name' => '所属ガーデン',
+        'grade' => '学年',
+        'position' => 'ガーデン役職',
+        'rank' => '序列',
+    ],
+    'legion' => [
+        'name' => '所属レギオン',
+        'position' => 'レギオン役職',
+    ],
     'introduction' => '公式紹介',
+    'cast' => 'キャスト',
     'miyadoc' => 'MiyaDoc記事名',
+    'remarks' => '特記事項',
 ];
