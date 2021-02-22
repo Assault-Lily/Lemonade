f9797fedddfa8fc407695cf8bc853094d75f2c0d
K Miyano <miyano@miyacorata.net>
2021-02-23 05:02:00 +0900

    Set Schema::defaultStringLength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
         if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){
             \URL::forceScheme('https');
         }
+        \Schema::defaultStringLength(191);
     }
 }
