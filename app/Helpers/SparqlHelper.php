93a1e82910cded5da1c2626e5b8ff3fcb7e44e53
K Miyano <miyano@miyacorata.net>
2021-03-17 04:26:39 +0900

    Add $predicateReplace option to sparqlQuery()

--- app/Helpers/SparqlHelper.php
+++ app/Helpers/SparqlHelper.php
@@ -2,25 +2,35 @@
 
 /**
  * @param $query string SPARQL Query
+ * @param $predicateReplace bool Replace predicate to prefixed string
  * @return object
  * @throws \Illuminate\Http\Client\RequestException
  * @throws \Illuminate\Http\Client\ConnectionException
  */
-function sparqlQuery(string $query): object
+function sparqlQuery(string $query, bool $predicateReplace = true): object
 {
 
-    return Http::timeout(3)->get(config('lemonade.sparqlEndpoint'), [
+    $res = Http::timeout(3)->get(config('lemonade.sparqlEndpoint'), [
         'format' => 'json',
         'query' => $query
-    ])->throw()->object();
+    ])->throw()->body();
+
+    if($predicateReplace){
+        foreach (config('lemonade.rdfPrefix') as $prefix => $uri){
+            $res = str_replace($uri, $prefix.':', $res);
+        }
+    }
+
+    return json_decode($res);
 
 }
 
 /**
  * @param $query string SPARQL Query
+ * @param $predicateReplace bool Replace predicate to prefixed string
  * @return object
  */
-function sparqlQueryOrDie(string $query): object
+function sparqlQueryOrDie(string $query, bool $predicateReplace): object
 {
     try {
         $res = sparqlQuery($query);
