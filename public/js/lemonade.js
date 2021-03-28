6cd4b6c4cd5f167a4965f073400b14fb789a467d
K Miyano <miyano@miyacorata.net>
2021-03-29 04:34:09 +0900

    Fix unexpected transition on debug mode

--- public/js/lemonade.js
+++ public/js/lemonade.js
@@ -1,5 +1,5 @@
 // 読み込み中に画面を隠すやつ
-document.querySelectorAll('a:not([target])').forEach((el)=>{
+document.querySelectorAll('a:not([target]):not(.sf-dump-toggle)').forEach((el)=>{
     el.addEventListener('click', ()=>{
         document.getElementsByTagName('body')[0].classList.add('hide');
     });
