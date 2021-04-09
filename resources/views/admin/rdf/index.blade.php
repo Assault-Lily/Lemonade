95caa1835e6380e8c2bab381bec5c27c3f451d58
K Miyano <miyano@miyacorata.net>
2021-04-10 03:03:34 +0900

    Add notice string (admin.rdf.index.blade)

--- resources/views/admin/rdf/index.blade.php
+++ resources/views/admin/rdf/index.blade.php
@@ -7,6 +7,9 @@
             <p>
                 SPARQLエンドポイントに問い合わせを行い、基本データについて同期を試みます
             </p>
+            <p style="color: darkred">
+                Ver5以降はSPARQLからデータをすべて取得可能であるため、基本データの同期は不要です。
+            </p>
             <div class="buttons three">
                 <a href="{{ route('admin.rdf.lily') }}" class="button">リリィ基本データ同期</a>
             </div>
