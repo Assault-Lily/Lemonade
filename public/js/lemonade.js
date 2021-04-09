d4bba42519d3801d37b2324ab9736b0dbce014cb
K Miyano <miyano@miyacorata.net>
2021-04-10 04:08:09 +0900

    Fix unexpected fade-out of page

--- public/js/lemonade.js
+++ public/js/lemonade.js
@@ -1,7 +1,7 @@
 // 読み込み中に画面を隠すやつ
 document.querySelectorAll('a:not([target]):not(.sf-dump-toggle)').forEach((el)=>{
-    el.addEventListener('click', ()=>{
-        document.getElementsByTagName('body')[0].classList.add('hide');
+    el.addEventListener('click', (e)=>{
+        if(!e.ctrlKey && !e.shiftKey) document.getElementsByTagName('body')[0].classList.add('hide');
     });
 });
 
