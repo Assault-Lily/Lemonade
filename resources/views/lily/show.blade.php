7d59ef8ee2e728d8119f55efeeb92141e1567223
K Miyano <miyano@miyacorata.net>
2021-05-18 02:47:43 +0900

    #アサルトリリィ_FA リンクの追加 (lily.show)

--- resources/views/lily/show.blade.php
+++ resources/views/lily/show.blade.php
@@ -309,6 +309,10 @@ $resource_qs = '?v'.explode(' ', config('lemonade.version'))[0];
                     <div style="text-align: center; padding-top: 130px; color: gray;">
                         <div style="margin-bottom: .4em">Image Unavailable</div>
                         <div class="buttons" style="font-size: smaller">
+                            <a href="https://twitter.com/search?q={{ urlencode('#アサルトリリィ_FA '.$triples[$ts]['schema:givenName'][0]) }}&f=live"
+                               target="_blank" class="button smaller">
+                                #アサルトリリィ_FA
+                            </a>
                             <a href="https://www.google.com/search?q={{ urlencode($triples[$ts]['schema:name'][0]) }}&tbm=isch"
                                target="_blank" class="button smaller">
                                 Google 画像検索
