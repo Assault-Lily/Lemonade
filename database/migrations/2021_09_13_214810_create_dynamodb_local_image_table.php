088644a7066adf597e4a2172cee16b32264d8488
fvh_P <ko_kurihara@yahoo.co.jp>
2021-09-17 21:33:07 +0900

    Revert "Revert "画像データ管理をローカルからDynamoDBへ移行""
    This reverts commit c7c969af8781226b623e81b8985dce3dcfa200dd.

---
+++ database/migrations/2021_09_13_214810_create_dynamodb_local_image_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use BaoPham\DynamoDb\DynamoDbClientService;
+
+class CreateDynamodbLocalImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // ローカル以外の時には実行しない
+        if (!app()->environment('local')) {
+            return;
+        }
+        $dynamoDbClientService = resolve(DynamoDbClientService::class);
+        $client = $dynamoDbClientService->getClient();
+
+        $params = [
+            'TableName' => 'Lemonade-images',
+            'KeySchema' => [
+                [
+                    'AttributeName' => 'id',
+                    'KeyType' => 'HASH',
+                ]
+            ],
+            'AttributeDefinitions' => [
+                [
+                    'AttributeName' => 'id',
+                    'AttributeType' => 'S'
+                ],
+            ],
+            'ProvisionedThroughput' => [
+                'ReadCapacityUnits' => 2,
+                'WriteCapacityUnits' => 1,
+            ],
+        ];
+
+        $client->createTable($params);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // ローカル以外の時には実行しない
+        if (!app()->environment('local')) {
+            return;
+        }
+        $dynamoDbClientService = resolve(DynamoDbClientService::class);
+        $client = $dynamoDbClientService->getClient();
+
+        $params = [
+            'TableName' => 'Lemonade-images',
+        ];
+
+        $client->deleteTable($params);
+    }
+}
