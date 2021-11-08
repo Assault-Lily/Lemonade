b95b3f5d13d90ddf149302ab7f800847d174ef82
K Miyano <miyano@miyacorata.net>
2021-11-09 00:10:14 +0900

    v9.0.1 Shichirigahama

--- config/lemonade.php
+++ config/lemonade.php
@@ -3,7 +3,7 @@
 return [
 
     /* Application Version */
-    'version' => '9.0.0 Shichirigahama',
+    'version' => '9.0.1 Shichirigahama',
 
     'webhooks' => [
         'discord-log' => env('DISCORD_URL'),
