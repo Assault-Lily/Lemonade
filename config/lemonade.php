e44116ad3f82252ff09067da5db81c97d9d05e49
K Miyano <miyano@miyacorata.net>
2022-08-01 22:13:37 +0900

    v13.0.1

--- config/lemonade.php
+++ config/lemonade.php
@@ -3,7 +3,7 @@
 return [
 
     /* Application Version */
-    'version' => '13.0.0 Yuigahama',
+    'version' => '13.0.1 Yuigahama',
 
     'webhooks' => [
         'discord-log' => env('DISCORD_URL'),
